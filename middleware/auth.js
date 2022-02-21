fd75d8e08641eacf473e849266554130d875df4b
takiyweb <kanytakiy@gmail.com>
2022-02-21 18:16:26 +0000

    Token expiry check implemented

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,6 +7,9 @@ module.exports = (req, res, next) => {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token provided.");
 
+  const isExpired = isTokenExpired(token);
+  if (isExpired) return res.status(401).send("Session expired. Please Login.");
+
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.user = decoded;
@@ -14,4 +17,13 @@ module.exports = (req, res, next) => {
   } catch (ex) {
     res.status(400).send("Invalid token.");
   }
+};
+
+const isTokenExpired = (token) => {
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  const expiry = payload?.exp;
+
+  if (!expiry) return true;
+
+  return Math.floor(Date.now() / 1000) >= expiry;
 };
